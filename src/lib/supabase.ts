c35867f4ea3ee47863a268121f329f849f950e47
A1VIPKORACODE <asenev87610@gmail.com>
2026-04-08 03:18:02 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,6 @@ export const SUPABASE_ANON_KEY = 'sb_publishable_NN9kjIyuK4dZMvzeIo0Fcw_iAp-FJsi
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-export const ADMIN_PASSWORD = 'AbanoubSamirRANDAHANY907&ANGLIabanoub907@#$';
 export const GIFT_URL = 'https://refpa3665.com/L?tag=d_2867569m_66335c_A1VIP&site=2867569&ad=66335';
 export const MELBET_TUTORIAL_URL = 'https://t.me/WIN_20K/253';
 export const CODE_USAGE_URL = 'https://t.me/A1VIP_KORA/59';
